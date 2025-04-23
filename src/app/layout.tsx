56ab99ed11250f4c9e150a1aa1ed7cefaa502498
Roibert PEñaloza <roibert.penaloza@commodo.co>
2025-04-23 13:53:44 +0000

    Add Web Analytics to the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import { Analytics } from '@vercel/analytics/next';
 // import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         
           {children}
+          <Analytics />
       </body>
     </html>
   )
